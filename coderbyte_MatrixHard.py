9ecd49092edca1f5d980da572a03cee6d7215490
Ali Ahsan Zaryab <56070572+Ahsanzaryab@users.noreply.github.com>
2022-02-13 03:02:22 +0500

    Update coderbyte_MatrixHard.py

--- coderbyte_MatrixHard.py
+++ coderbyte_MatrixHard.py
@@ -4,21 +4,14 @@ def MaximalSquare(strParam):
     c = len(strParam[0])
     s = min(r, c)
     
-    for x in range(s, 0, -1):  # descending searching
-        for i in range(0, r-x+1):    # from r to r (filter goes from upper to bottom)   
-            # if x == r, do this for loop only once
-            # i is the r index of the matrix
-            for j in range(0, c-x+1): # from column to column (filter goes from left to right)
-                # if x == c, do this loop only once
-                # j is the column index of the matrix
-                # strParam[i][j] is the upper left corner of the submatrix
+    for x in range(s, 0, -1):  
+        for i in range(0, r-x+1):    
+            for j in range(0, c-x+1): 
                 prod = 1
                 
-                # S[m][n] is the elements of the submatrix
                 for m in range(i, x+i):
                     for n in range(j, x+j):
                         prod *= S[m][n]
-                ## after all the prod operator:
                 if prod == 1:
                     return x*x
     return 0
